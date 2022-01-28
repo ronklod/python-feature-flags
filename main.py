be0b2a9d92c5646463cffc42d393029755fe413b
ronklod <ronklod@gmail.com>
2022-01-28 10:23:35 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from UnleashClient import UnleashClient
+from time import time, sleep
+
+#setting up unleash connnection
+def setupUnleash():
+    client = UnleashClient(
+        url="http://localhost:4242/api/",
+        app_name="my-python-app",
+        custom_headers={'Authorization': '*:default.f741b301a8a09519e8311b62c0423d7cdc3c9483d172e2f2e4f1ef11'})
+
+    client.initialize_client()
+    return client
+
+#checking every second the status of the python flag
+def unleashFT(client):
+    while True:
+        sleep(1 - time() % 1)  # run every 1 second... you can change that
+        print("python flag - stnadard startegy -" +  str(client.is_enabled("python_flag")))
+        print("experiment flag - traffic base strategy -" + str(client.is_enabled("experiment_flag")))
+
+
+def run():
+    unleashFT(setupUnleash())
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    run()
+
+
+
+
+
